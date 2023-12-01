02e725b85332951686aa963052712c992c77abec
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2023-12-01 17:36:11 -0500

    Crud Realms Roles and Client Roles

--- src/rol/application/rol.controller.ts
+++ src/rol/application/rol.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { RolService } from '../infrastructure/rol.service';
 import { Response } from 'express';
 import { RolCreate } from '../domain/rol.dto';
@@ -8,15 +17,123 @@ export class RolController {
   constructor(private readonly rolService: RolService) {}
 
   @Get()
-  async getRoles() {
-    return await this.rolService.getRoles();
+  async getRoles(@Res() res: Response) {
+    try {
+      const response = await this.rolService.getRoles();
+      res.status(response.status).json(response.data);
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
   }
 
   @Post()
   async createRole(@Body() rolCreate: RolCreate, @Res() res: Response) {
-    const response = await this.rolService.createRoles(rolCreate);
-    res
-      .status(Number(response.status))
-      .send(response.status === 201 ? 'Rol creado' : 'No se pudo crear el rol');
+    try {
+      const response = await this.rolService.createRoles(rolCreate);
+      res.status(response.status).send('Rol creado');
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Put(':rolename')
+  async editRole(
+    @Param('rolename') rolename: string,
+    @Body() rolEdit: RolCreate,
+    @Res() res: Response,
+  ) {
+    try {
+      const response = await this.rolService.editRol(rolEdit, rolename);
+      res
+        .status(response.status)
+        .send(
+          response.status === 204
+            ? 'Rol actualizado'
+            : 'No se pudo editar el rol',
+        );
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Delete(':rolename')
+  async deleteRole(@Param('rolename') rolename: string, @Res() res: Response) {
+    try {
+      const response = await this.rolService.deleteRol(rolename);
+      res.status(response.status).send('Rol eliminado');
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Post('clientRole/:client_id')
+  async createRoleClient(
+    @Param('client_id') client_id: string,
+    @Res() res: Response,
+    @Body() rol: RolCreate,
+  ) {
+    try {
+      const response = await this.rolService.createRolToClient(rol, client_id);
+      res.status(response.status).send('Rol Creado');
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Get('clientRole/:client_id')
+  async getRolesClient(
+    @Param('client_id') client_id: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const response = await this.rolService.getRolesClient(client_id);
+      res.status(response.status).json(response.data);
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Put('clientRole/:client_id/role/:role_name')
+  async editRolClient(
+    @Param('client_id') client_id: string,
+    @Param('role_name') role_name: string,
+    @Body() rol: RolCreate,
+    @Res() res: Response,
+  ) {
+    try {
+      const response = await this.rolService.editRolClient(
+        rol,
+        client_id,
+        role_name,
+      );
+      res.status(response.status).send('Rol Actualizado');
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
+  }
+
+  @Delete('clientRole/:client_id/role/:role_name')
+  async deleteRolClient(
+    @Param('client_id') client_id: string,
+    @Param('role_name') role_name: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const response = await this.rolService.deleteRolClient(
+        client_id,
+        role_name,
+      );
+      res.status(response.status).send('Rol Eliminado');
+    } catch (e: any) {
+      const { errorMessage } = e.response.data;
+      res.status(e.response.status).send(errorMessage);
+    }
   }
 }
--- src/rol/infrastructure/rol.service.ts
+++ src/rol/infrastructure/rol.service.ts
@@ -15,7 +15,10 @@ export class RolService {
   }
   private _url = `${this.configService.get(
     'AUTH_SERVER_URL',
-  )}/realms/${this.configService.get('REALM')}/roles`;
+  )}/admin/realms/${this.configService.get('REALM')}/roles`;
+  private _url_client = `${this.configService.get(
+    'AUTH_SERVER_URL',
+  )}/admin/realms/${this.configService.get('REALM')}/clients`;
   constructor(private readonly configService: ConfigService) {}
 
   private async login_admin(): Promise<any> {
@@ -25,7 +28,7 @@ export class RolService {
       this.configService.get('AUTH_SERVER_URL'),
     );
   }
-  async getRoles(): Promise<string> {
+  async getRoles(): Promise<any> {
     const headers = await this.getHeadersAdmin();
     return await axios.get(this._url, { headers });
   }
@@ -41,5 +44,52 @@ export class RolService {
   async createRoles(rol: RolCreate): Promise<any> {
     const headers = await this.getHeadersAdmin();
     return await axios.post(this._url, rol, { headers });
+  }
+
+  async editRol(rol: RolCreate, old_name: string): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.put(`${this._url}/${old_name}`, rol, { headers });
+  }
+
+  async deleteRol(rolName: string): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.delete(`${this._url}/${rolName}`, { headers });
+  }
+
+  async createRolToClient(rol: RolCreate, client_id: string): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.post(`${this._url_client}/${client_id}/roles`, rol, {
+      headers,
+    });
+  }
+
+  async getRolesClient(client_id: string): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.get(`${this._url_client}/${client_id}/roles`, {
+      headers,
+    });
+  }
+
+  async editRolClient(
+    rol: RolCreate,
+    client_id: string,
+    role_name: string,
+  ): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.put(
+      `${this._url_client}/${client_id}/roles/${role_name}`,
+      rol,
+      {
+        headers,
+      },
+    );
+  }
+
+  async deleteRolClient(client_id: string, role_name: string): Promise<any> {
+    const headers = await this.getHeadersAdmin();
+    return await axios.delete(
+      `${this._url_client}/${client_id}/roles/${role_name}`,
+      { headers },
+    );
   }
 }
